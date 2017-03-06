dfbefae4b31b77a1d442a0a5cc817ff0e38b933c
Alexander-Step <alexander.sergeevich.s@gmail.com>
2017-03-06 11:24:02 +0300

    Update

--- Timezone Reminder/WeatherClient.h
+++ Timezone Reminder/WeatherClient.h
@@ -10,22 +10,27 @@
 #import "CLLocation+APTimeZones.h"
 #import "CallVCr.h"
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @protocol WeatherClientDelegate;
 
 @interface WeatherClient : AFHTTPSessionManager
 
-@property (nonatomic, weak) id <WeatherClientDelegate> delegate;
+NS_ASSUME_NONNULL_BEGIN
+
+@property (nonatomic, weak, nullable) id <WeatherClientDelegate> delegate;
+@property (nonnull, strong) UIAlertController *alert;
 
 + (WeatherClient*)sharedWeatherClient;
-- (instancetype)initWithBaseURL:(NSURL*) url;
-- (void)updateWeatherAtLocation:(CLLocation*)location forDate:(NSDate*)date forCall:(Call*)call forWho:(NSString*)who; //who="user" or "client"
-
+- (instancetype)initWithBaseURL:(nullable NSURL*) url;
+- (void)updateWeatherAtLocation:(CLLocation*)location forDate:(NSDate*)date forCall:(Call*)call forWho:( NSString*)who; //who="user" or "client"
+NS_ASSUME_NONNULL_END
 @end
 
 @protocol WeatherClientDelegate <NSObject>
 @optional
-- (void) weatherClient:(WeatherClient*)client didUpdateWithWeather:(id)weather forCall:(Call*)call forWho:(NSString*)who;
-- (void)weatherClient:(WeatherClient*)client didFailWithError:(NSError*)error;
+- (void) weatherClient:(nullable WeatherClient*)client didUpdateWithWeather:(nullable id)weather forCall:(nullable Call*)call forWho:(nullable NSString*)who;
+- (void)weatherClient:(nullable WeatherClient*)client didFailWithError:(nullable NSError*)error;
+
 
 @end
--- Timezone Reminder/WeatherClient.m
+++ Timezone Reminder/WeatherClient.m
@@ -8,7 +8,7 @@
 
 #import "WeatherClient.h"
 
-static NSString * const WorldWeatherOnlineAPIKey = @"3a632d5dc9e64c41817110826172702";
+static NSString * const WorldWeatherOnlineAPIKey = @"API KEY";
 static NSString * const WorldWeatherOnlineURLString = @"https://api.worldweatheronline.com/premium/v1/";
 //static NSString * const WorldWeatherOnlineURLString = @"http://api.worldweatheronline.com/free/v1/";
 
@@ -39,33 +39,44 @@ static NSString * const WorldWeatherOnlineURLString = @"https://api.worldweather
 
 - (void)updateWeatherAtLocation:(CLLocation*)location forDate:(NSDate*)date forCall:(Call*)call forWho:(NSString*)who //who="user" or "client"
 {
-    //build parameters
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    formatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
-    formatter.dateFormat = @"yyyy-MM-dd";
-    formatter.timeZone = [[APTimeZones sharedInstance] timeZoneWithLocation:location];
-    NSString *requestDateString = [formatter stringFromDate:date];
-    
-    NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
-    parameters[@"q"] = [NSString stringWithFormat:@"%f,%f",location.coordinate.latitude,location.coordinate.longitude];
-    parameters[@"format"]=@"json";
-    parameters[@"key"] = WorldWeatherOnlineAPIKey;
-    parameters[@"date"] = requestDateString;
-    
-    //start a fetch
-    __weak Call* weakCall = call;
-    
-    [self GET:@"weather.ashx" parameters:parameters progress:nil
-      success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-          if ([self.delegate respondsToSelector:@selector(weatherClient:didUpdateWithWeather:forCall:forWho:)]){
-              [self.delegate weatherClient:self didUpdateWithWeather:responseObject forCall:weakCall forWho:who];
-          }
-          
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        if ([self.delegate respondsToSelector:@selector(weatherClient:didFailWithError:)]){
-            [self.delegate weatherClient:self didFailWithError:error];
+    if ([WorldWeatherOnlineAPIKey isEqualToString:@"API KEY"]){
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Input API KEY" message:@"Please, input your API key for World Weather Online API to use it's functions" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *actionOK = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        [alert addAction:actionOK];
+        if ([self.delegate respondsToSelector:@selector(presentViewController:animated:completion:)]){
+            CallVCr *callVCrPointer = (CallVCr*) self.delegate;
+            [callVCrPointer presentViewController:alert animated:YES completion:nil];
         }
-    }];
+        
+    } else {
+        //build parameters
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+        formatter.dateFormat = @"yyyy-MM-dd";
+        formatter.timeZone = [[APTimeZones sharedInstance] timeZoneWithLocation:location];
+        NSString *requestDateString = [formatter stringFromDate:date];
+        
+        NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
+        parameters[@"q"] = [NSString stringWithFormat:@"%f,%f",location.coordinate.latitude,location.coordinate.longitude];
+        parameters[@"format"]=@"json";
+        parameters[@"key"] = WorldWeatherOnlineAPIKey;
+        parameters[@"date"] = requestDateString;
+        
+        //start a fetch
+        __weak Call* weakCall = call;
+        
+        [self GET:@"weather.ashx" parameters:parameters progress:nil
+          success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+              if ([self.delegate respondsToSelector:@selector(weatherClient:didUpdateWithWeather:forCall:forWho:)]){
+                  [self.delegate weatherClient:self didUpdateWithWeather:responseObject forCall:weakCall forWho:who];
+              }
+              
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            if ([self.delegate respondsToSelector:@selector(weatherClient:didFailWithError:)]){
+                [self.delegate weatherClient:self didFailWithError:error];
+            }
+        }];
+    }
 }
 
 @end
